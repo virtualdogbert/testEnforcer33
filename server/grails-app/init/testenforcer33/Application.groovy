dda9f01174ee4698c334df918b82b74dc4df22b7
Software Engineering  Geek <virtualdogbert@gmail.com>
2018-06-11 22:10:52 -0400

    Setting cookie properties for session id http only/secure . Also setting the cookie name.

--- server/grails-app/init/testenforcer33/Application.groovy
+++ server/grails-app/init/testenforcer33/Application.groovy
@@ -2,9 +2,23 @@ package testenforcer33
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.boot.web.servlet.ServletContextInitializer
 
-class Application extends GrailsAutoConfiguration {
+import javax.servlet.ServletContext
+import javax.servlet.ServletException
+import javax.servlet.SessionCookieConfig
+
+class Application extends GrailsAutoConfiguration implements ServletContextInitializer {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
+    }
+
+    @Override
+    void onStartup(ServletContext servletContext) throws ServletException {
+        SessionCookieConfig config = servletContext.getSessionCookieConfig()
+
+        config.setName('sid')
+        config.httpOnly = true
+        config.secure = true
     }
 }
