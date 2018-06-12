cc6b7824d6d10ea83e2d6cf00a66aba3b3f549e1
Software Engineering  Geek <virtualdogbert@gmail.com>
2018-06-11 22:11:34 -0400

    Adding default urls so that I can render gsp pages.

--- server/grails-app/controllers/testenforcer33/UrlMappings.groovy
+++ server/grails-app/controllers/testenforcer33/UrlMappings.groovy
@@ -3,15 +3,21 @@ package testenforcer33
 class UrlMappings {
 
     static mappings = {
-        delete "/$controller/$id(.$format)?"(action:"delete")
-        get "/$controller(.$format)?"(action:"index")
-        get "/$controller/$id(.$format)?"(action:"show")
-        post "/$controller(.$format)?"(action:"save")
-        put "/$controller/$id(.$format)?"(action:"update")
-        patch "/$controller/$id(.$format)?"(action:"patch")
+        delete "/$controller/$id(.$format)?"(action: "delete")
+        get "/$controller(.$format)?"(action: "index")
+        get "/$controller/$id(.$format)?"(action: "show")
+        post "/$controller(.$format)?"(action: "save")
+        put "/$controller/$id(.$format)?"(action: "update")
+        patch "/$controller/$id(.$format)?"(action: "patch")
 
-        "/"(controller: 'application', action:'index')
+        "/"(controller: 'application', action: 'index')
         "500"(view: '/error')
         "404"(view: '/notFound')
+
+        "/$controller/$action?/$id?(.$format)?" {
+            constraints {
+                // apply constraints here
+            }
+        }
     }
 }
