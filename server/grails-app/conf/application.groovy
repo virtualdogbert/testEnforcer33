f7ca64a8987311391eff964a6197df0a2698e3cb
Software Engineering  Geek <virtualdogbert@gmail.com>
2018-06-26 22:21:25 -0400

    Adding db console

--- server/grails-app/conf/application.groovy
+++ server/grails-app/conf/application.groovy
@@ -123,6 +123,9 @@ dataSource {
 environments {
 
     development {
+        grails.dbconsole.enabled = true
+        grails.dbconsole.urlRoot = '/admin/dbconsole'
+
         dataSource {
             dbCreate = 'create-drop'
             url = 'jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE'
